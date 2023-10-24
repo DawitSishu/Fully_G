80d6a7502e370afb27415e93d9946e903638024f
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-24 14:07:55 -0400

    some upoad images

--- app/lib/Pages/AddMemoryPage.dart
+++ app/lib/Pages/AddMemoryPage.dart
@@ -1,3 +1,4 @@
+import 'package:app/util/widgets.dart';
 import 'package:flutter/material.dart';
 
 class AddMemory extends StatefulWidget {
@@ -30,8 +31,138 @@ class _AddMemoryState extends State<AddMemory> {
         ),
       ),
       // Icons.favorite
-      body: Center(
-        child: Text("Upload page"),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            // borderRadius: BorderRadius.circular(5),
+            onTap: () async {},
+            child: Container(
+              // color: Colors.grey,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: Colors.grey.shade300,
+                ),
+                color: Colors.grey.shade100,
+              ),
+              height: 190,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Image.asset('assets/images/ImportPdf.png'),
+
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_a_photo_sharp),
+                    color: Color.fromARGB(255, 116, 59, 107),
+                    iconSize: 50,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "Select Image",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Add Title',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Add Description',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+                onPressed: () async {},
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color.fromARGB(255, 116, 59, 107),
+                        Color.fromARGB(255, 100, 58, 97)
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    constraints: const BoxConstraints(
+                      maxWidth: 200,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Save',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
