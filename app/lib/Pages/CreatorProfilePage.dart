b7dd6a5b743be86ad9510eb99be6649425093a5c
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-26 14:01:26 -0400

    not much just a bunch of errorx

--- app/lib/Pages/CreatorProfilePage.dart
+++ app/lib/Pages/CreatorProfilePage.dart
@@ -12,78 +12,83 @@ class _CreatorProfileState extends State<CreatorProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
+      body: Column(
         children: [
-          // Background Image with Rounded Edges
-          Container(
-            height: 350, // Adjust the height as needed
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                    'assets/images/profile.png'), // Replace with your image
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30.0),
-                bottomRight: Radius.circular(30.0),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 200, // Adjust the top position for your content
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.white, // Background color of the profile page
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+          Stack(
+            children: [
+              // Background Image with Rounded Edges
+              Container(
+                height: 350, // Adjust the height as needed
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/profile.png'), // Replace with your image
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
                 ),
               ),
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  // Profile Icon and Name
-                  Row(
+              Positioned(
+                top: 200, // Adjust the top position for your content
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Background color of the profile page
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                  ),
+                  child: Column(
                     children: [
-                      // Profile Icon (CircleAvatar)
-                      CircleAvatar(
-                        radius: 30, // Adjust the size as needed
-                        backgroundColor: Color.fromARGB(
-                            255, 116, 59, 107), // Background color of the icon
-                        child: Icon(
-                          Icons
-                              .account_circle, // Replace with your desired icon
-                          size: 60, // Adjust the size of the icon
-                          color: Colors.white, // Color of the icon
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      // Profile Name and Bio
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Row(
                         children: [
-                          Text(
-                            "Full Name",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Color.fromARGB(255, 116, 59, 107),
+                            child: Icon(
+                              Icons.account_circle,
+                              size: 60,
+                              color: Colors.white,
                             ),
                           ),
-                          Text("Creating Memories Together"),
+                          SizedBox(width: 20),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Full Name",
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("Creating Memories Together"),
+                            ],
+                          ),
                         ],
                       ),
                     ],
                   ),
-
-                  // Add more profile-related ListTile widgets
-                ],
+                ),
               ),
-            ),
+            ],
           ),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
+          Text("Creating Memories Together"),
         ],
       ),
     );
@@ -91,4 +96,4 @@ class _CreatorProfileState extends State<CreatorProfile> {
 }
 
 
-//dave fix the error and this page mfr
+// dave try to fix this
