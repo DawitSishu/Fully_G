22badd37435f1bbcc41024ba07de29d12cf76549
DawitSishu <dawitalemayhu55@gmail.com>
2023-11-20 22:30:40 -0500

    added the receiver route

--- server/Routes/receiverRoute.js
+++ server/Routes/receiverRoute.js
@@ -1,13 +1,20 @@
 import express from "express";
-import { getAudio, getImages } from "../Controllers/receiverController.js";
+import {
+  getAudio,
+  getImages,
+  receiveGift,
+} from "../Controllers/receiverController.js";
 import userAuthChecker from "../MiddleWares/UserAuthChecker.js";
 
 const receiverRouter = express.Router();
 
 //receive audio
-receiverRouter.get("/audio/:id", userAuthChecker,getAudio);
+receiverRouter.get("/audio/:id", userAuthChecker, getAudio);
 
 //receive photo
-receiverRouter.get("/images/:id",userAuthChecker, getImages);
+receiverRouter.get("/images/:id", userAuthChecker, getImages);
+
+//receivegift
+receiverRouter.get("/gift", userAuthChecker, receiveGift);
 
 export default receiverRouter;
