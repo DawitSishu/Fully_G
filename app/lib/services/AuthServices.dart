30d320b8b13858e81c2cf6801d1065d395b71a7d
DawitSishu <dawitalemayhu55@gmail.com>
2023-12-11 09:23:50 -0500

    token instantoation is now done

--- app/lib/services/AuthServices.dart
+++ app/lib/services/AuthServices.dart
@@ -1,6 +1,26 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<String?> getToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('token');
+}
+
+Future<String?> getUserId() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('userId');
+}
+
+Future<String?> getPhone() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('phone');
+}
+
+Future<String?> getLoveId() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('loveId');
+}
 
 void fetchData() async {
   var url = 'http://192.168.42.56:5000';
@@ -78,8 +98,13 @@ Future LogIn(bodyData) async {
       headers: headers,
       body: json.encode(bodyData),
     );
+    final prefs = await SharedPreferences.getInstance();
     final data = json.decode(response.body);
     if (response.statusCode == 200 || response.statusCode == 201) {
+      await prefs.setString('token', data['token'].toString());
+      await prefs.setString('userId', data['userId'].toString());
+      await prefs.setString('phone', data['phone'].toString());
+      await prefs.setString('loveId', data['love_id'].toString());
       return {"success": true, "data": data};
     } else {
       return {"success": false, "data": data};
