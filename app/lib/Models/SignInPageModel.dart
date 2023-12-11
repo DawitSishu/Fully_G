ef6d468aecd7aaf215d679436db7eba0bf5d5859
DawitSishu <dawitalemayhu55@gmail.com>
2023-12-11 08:56:30 -0500

    now phone is to be converted

--- app/lib/Models/SignInPageModel.dart
+++ app/lib/Models/SignInPageModel.dart
@@ -5,7 +5,19 @@ class SignInData {
 
   checkSignInData() => phone.isEmpty || password.isEmpty;
 
+  convertPhone() {
+    if (phone.startsWith('0')) {
+      phone = '+251${phone.substring(1)}';
+      return;
+    }
+    if (phone.startsWith('2')) {
+      phone = '+$phone';
+      return;
+    }
+  }
+
   printAndGetData() {
+    convertPhone();
     print({"phone": phone, "password": password});
     return {"phone": phone, "password": password};
   }
